fcfdf00a1f244d915dff1939ab93a71cf525aed8
mgan2014 <mason.gan@gmail.com>
2022-04-14 22:12:23 -0400

    add the collobrative filtering

--- ML_ex8/cofiCostFunc.m
+++ ML_ex8/cofiCostFunc.m
@@ -42,19 +42,14 @@ Theta_grad = zeros(size(Theta));
 
 
 
+%J = 1/2 * sum(sum((X * Theta' - Y).^2.*R));
+J = 1/2 * sum(sum((X * Theta' - Y).^2.*R)) + lambda/2 * sum(sum(Theta.^2)) ...
+   + lambda/2 * sum(sum(X.^2));
 
-
-
-
-
-
-
-
-
-
-
-
-
+%X_grad = ((X * Theta' - Y) .* R) * Theta;
+%Theta_grad = ((X * Theta' - Y) .* R)' * X; 
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda * Theta; 
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
